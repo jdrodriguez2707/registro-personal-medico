4b1a6f59b46917bfe8a6dc4473c76915f535e0e2
jdrodriguez2707 <johandavid.jdr@gmail.com>
2024-06-29 09:44:33 -0500

    Update database

--- database/registro_personal_medico.sql
+++ database/registro_personal_medico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 28, 2024 at 03:28 PM
+-- Generation Time: Jun 29, 2024 at 10:43 AM
 -- Server version: 8.0.37-0ubuntu0.22.04.3
 -- PHP Version: 8.1.2-1ubuntu2.18
 
@@ -31,20 +31,20 @@ CREATE TABLE `personal_medico` (
   `id` int NOT NULL,
   `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `apellido` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `cargo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
+  `cargo` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `estado` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'activo'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `personal_medico`
 --
 
-INSERT INTO `personal_medico` (`id`, `nombre`, `apellido`, `cargo`) VALUES
-(1, 'Coeli', 'Corzo', 'Nutricionista'),
-(2, 'Sharet', 'Vargas', 'Fisioterapeuta'),
-(3, 'Jimmy', 'Tejedor', 'Farmacéutico'),
-(4, 'Johan', 'Rodriguez', 'Paramédico'),
-(6, 'Miguel', 'Salomon', 'Médico General'),
-(7, 'Gustavo', 'Gómez', 'Psicólogo');
+INSERT INTO `personal_medico` (`id`, `nombre`, `apellido`, `cargo`, `estado`) VALUES
+(1, 'Coeli', 'Corzo', 'Nutricionista', 'activo'),
+(2, 'Sharet', 'Vargas', 'Fisioterapeuta', 'activo'),
+(3, 'Jimmy', 'Tejedor', 'Farmacéutico', 'activo'),
+(4, 'Johan', 'Rodriguez', 'Paramédico', 'activo'),
+(33, 'Diego', 'Padilla', 'Endocrinologo', 'inactivo');
 
 -- --------------------------------------------------------
 
@@ -66,12 +66,13 @@ CREATE TABLE `registro_ingresos_salidas` (
 INSERT INTO `registro_ingresos_salidas` (`id`, `id_empleado`, `fecha_hora_ingreso`, `fecha_hora_salida`) VALUES
 (1, 1, '2024-06-28 08:00:00', '2024-06-28 17:00:00'),
 (2, 3, '2024-06-13 09:00:00', '2024-06-28 20:00:00'),
-(3, 2, '2024-07-01 07:00:00', '2024-06-28 18:00:00'),
 (4, 4, '2024-07-01 08:00:00', '2024-06-28 19:00:00'),
 (5, 1, '2024-07-02 08:00:00', '2024-06-28 22:00:00'),
 (8, 1, '2024-06-28 18:44:00', '2024-06-28 01:44:00'),
-(9, 4, '2024-06-28 07:49:00', '2024-06-28 01:53:00'),
-(15, 7, '2024-06-28 14:30:00', '2024-06-28 23:30:00');
+(35, 6, '2024-06-28 23:40:00', '2024-06-29 07:40:00'),
+(36, 29, '2024-06-28 22:40:00', '2024-06-29 07:40:00'),
+(40, 33, '2024-06-28 23:15:00', '2024-06-29 06:15:00'),
+(42, 33, '2024-06-30 23:15:00', '2024-07-01 09:15:00');
 
 --
 -- Indexes for dumped tables
@@ -98,23 +99,13 @@ ALTER TABLE `registro_ingresos_salidas`
 -- AUTO_INCREMENT for table `personal_medico`
 --
 ALTER TABLE `personal_medico`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `registro_ingresos_salidas`
 --
 ALTER TABLE `registro_ingresos_salidas`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `registro_ingresos_salidas`
---
--- ALTER TABLE `registro_ingresos_salidas`
---  ADD CONSTRAINT `registro_ingresos_salidas_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `personal_medico` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
